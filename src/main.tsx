5a4545365040c2e560d81a99f2b7dfb476ac7ff2
Rishabh <rishabh@swaransoft.com>
2025-11-01 14:46:56 +0530

    Initial commit - fresh setup under Rishabh's account

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles/brand.css';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+
